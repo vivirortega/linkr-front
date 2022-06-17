51fd9376f3a0f8ee112a77996c26262c90a687af
Rafael <rafinha_fern@hotmail.com>
2022-06-17 15:46:07 -0300

    feat: rendering array from server.

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,27 +1,40 @@
 import { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../../contexts/socket";
+import { Input,InputDiv,List } from './style.js';
+import {ReactComponent as SearchSvg} from '../assets/svg/search-outline.svg'
 
 export default function Search() {
     const socket = useContext(SocketContext);
     const [target,setTarget] = useState('');
-    const [result,setResult] = useState({});
+    const [result,setResult] = useState([]);
     useEffect(() => {
         socket.on('search_result',(resultServer) => {
             setResult(resultServer)
-            console.log(resultServer)
            })
     },[]);
     useEffect(() =>{
         socket.emit('search-value',target)
     },[target])
     return (
-        <input
+        <InputDiv>
+        <Input
             type="text"
             placeholder="Search users..."
             value={target}
             onChange={(e) => setTarget(e.target.value)}
             required
           />
+          <List display={result===[]?'none':'initial'}>
+            {result?.map((user,index) => {
+                return (
+                    <div className="user">
+                        <img src={user.url} alt={user.url} key={index}/>
+                        <p>{user.user_name}</p></div>
+                )
+            })}
+          </List>
+          <SearchSvg/>
+          </InputDiv>
         
     )
 }
