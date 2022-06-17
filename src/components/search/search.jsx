c58765d37df3e9b95e2d0a1c55ef110ab337b803
Rafael <rafinha_fern@hotmail.com>
2022-06-17 14:14:24 -0300

    feat: recive search filter by console.log

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,16 +1,27 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../../contexts/socket";
 
 export default function Search() {
     const socket = useContext(SocketContext);
+    const [target,setTarget] = useState('');
+    const [result,setResult] = useState({});
     useEffect(() => {
-       socket.on('connect',(skt) => {
-        console.log('alo')
-       })
-        
-    }, [])
+        socket.on('search_result',(resultServer) => {
+            setResult(resultServer)
+            console.log(resultServer)
+           })
+    },[]);
+    useEffect(() =>{
+        socket.emit('search-value',target)
+    },[target])
     return (
-        <input></input>
+        <input
+            type="text"
+            placeholder="Search users..."
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+            required
+          />
         
     )
 }
