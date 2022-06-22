7dd5a91bf7cf8ec4c30d7d5375fcdac0ac7223fe
unverzed <vitoriarortega@gmail.com>
2022-06-22 01:37:21 -0300

    feat: add function to insert follow

--- src/components/userPage/userPage.jsx
+++ src/components/userPage/userPage.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../header/header";
 import Posts from "../posts/posts";
+import UserContext from "../../contexts/usercontext";
 import { ContentWrapper, Main, MainWrapper, TrendingWrapper } from "../timeline/style";
 import { Follow, Unfollow } from "./style";
 import Trending from "../Trending/Trending";
@@ -12,6 +13,15 @@ export default function UserPage() {
     const {id} = useParams();
     const [name,setName] = useState('');
     const [image,setImg] = useState('');
+    const [following, setFollowing] = useState(null);
+    const [isLoadingFollow, setIsLoadingFollow] = useState(false);
+    const { token, user } = useContext(UserContext);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
     useEffect(()=> {
       const promise = axios.get(process.env.REACT_APP_API_URL+'/user/'+id);
       promise.then(response => {
@@ -20,6 +30,25 @@ export default function UserPage() {
         setImg(user.url)
       })
     })
+    
+    function insertFollow(){
+      const data = {
+        follower: user.id,
+        following: id
+      }
+
+      const promise = axios.post("http://localhost:5000/follows", data, config);
+
+      promise.then((response) => {
+        setFollowing(response);
+        console.log(response);
+      })
+
+      promise.catch((error) => {
+        setIsLoadingFollow(false);
+        console.log('erro ao seguir', user);
+      })
+    }
 
     return (
       <>
@@ -28,7 +57,7 @@ export default function UserPage() {
         <MainWrapper>
           <div className="user-timeline">
           <h1> <img src={image} alt={`${name} profile.`}/> {`${name}'s timeline`}</h1>
-          <Follow>Follow</Follow>
+          <Follow disabled={isLoadingFollow} onClick={insertFollow}>Follow</Follow>
           </div>
           <TrendingWrapper>
             <ContentWrapper>
